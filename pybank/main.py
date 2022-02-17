be0c3c71b00b37eb92abea4d17c4bad432143341
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-02-17 23:56:00 +1100

    initial attempt

--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,30 @@
+import os
+import csv
+
+readfile = os.path.join("resources", "budget_data.csv")
+outputfile = os.path.join("analysis", "budget_analysis.csv")
+
+datecol = []
+plval = []
+
+with open(readfile, "r") as budget_datafile:
+    csvreader = csv.reader(budget_datafile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        datecol.append(row[0])
+        plval.append(int(row[1]))
+    sumplval = "${:.0f}".format(sum(plval))
+    countmth = len(datecol)
+
+    print("Finacial Analysis")
+    print("-------------------------------------------")
+    print(f"Total Months: {countmth}")
+    print(f"Total: {sumplval}")
+
+with open(outputfile, "w") as budget_analysisfile:
+    writer = csv.writer(budget_analysisfile)
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["Total Months:", countmth])
+    writer.writerow(["Total:", sumplval])
 
