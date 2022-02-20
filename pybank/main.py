41bc773746919d0acc9e8e40bbb9faccf8c1f51e
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-02-20 20:23:51 +1100

    Script completed

--- pybank/main.py
+++ pybank/main.py
@@ -56,7 +56,7 @@ with open(readfile, "r") as budget_datafile:
     print(f"Greatest Decrease in Profits: {minDecreaseMonth} ({minDecrease})")
 
 # Write output to file 
-with open(outputfile, "w", encoding='UTF8') as budget_analysisfile:
+with open(outputfile, "w", encoding='UTF8', newline='') as budget_analysisfile:
     writer = csv.writer(budget_analysisfile)
     writer.writerow(["Financial Analysis"])
     writer.writerow(["Total Months:", countmth])
