92ec52a210ab6ea113accd376479a1839029f534
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-02-21 00:06:41 +1100

    write to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,15 @@
+from cgitb import text
 import os
 import csv
 
 sourcefile = os.path.join("Resources", "election_data.csv")
-outputfile = os.path.join("analysis", "election_analysis.csv")
+outputfile = os.path.join("analysis", "election_analysis.txt")
 
 voterList = []
-countyList = []
 VcandidateList = []
 numVotePerCandidate = []
+
+outCandidate = []
 
 with open(sourcefile, "r") as datafile:
     csvreader = csv.reader(datafile, delimiter=',')
@@ -15,39 +17,59 @@ with open(sourcefile, "r") as datafile:
 
     for row in csvreader:
         voterList.append(row[0])
-        countyList.append(row[1])
+#        countyList.append(row[1])
         VcandidateList.append(row[2])
     numVoters = len(voterList)
 
-    print("Election Results")
-    print("-----------------------------------")    
-    print(f"Total Votes: {numVoters}")
-    print("-----------------------------------")
+    outputHeading = "Election Results"
+    lineBreak = "-----------------------------------"
+    line01Text = "Total Votes: "
+    line99Text = "Winner: "
+
+    print(outputHeading)
+    print(lineBreak)    
+    print(f"{line01Text} {numVoters}")
+    print(lineBreak)
+    
     # Remove duplicates from candidate list
     candidateList = list(dict.fromkeys(VcandidateList))
-    
+
+# Write to analysis file
+with open(outputfile, "w") as ptext:
+    ptext.writelines(outputHeading)
+    ptext.write('\n')
+    ptext.writelines(lineBreak)
+    ptext.write('\n')
+    outputLine = f"{line01Text} {numVoters}"
+    ptext.writelines(outputLine)
+    ptext.write('\n')
+    ptext.writelines(lineBreak)
+    ptext.write('\n')
+
  #   print(len(candidateList))
     for candidate in candidateList:
         numVote = VcandidateList.count(candidate)
         numVotePerCandidate.append(numVote)
         VotePercent = numVote/numVoters
         VotePercent = "{:.3%}".format(VotePercent)
-        print(f"{candidate}: {VotePercent} ({numVote})")
-    print("-----------------------------------")     
+
+        outputLine = f"{candidate}: {VotePercent} ({numVote})"
+        print(outputLine)
+        ptext.writelines(outputLine)
+        ptext.write('\n')
+    print(lineBreak)
+    ptext.writelines(lineBreak)     
+    ptext.write('\n')
 
     winner = max(numVotePerCandidate)
     winner = numVotePerCandidate.index(winner)
     winner = candidateList[winner]
-    print(f"Winner: {winner}")
-    print("-----------------------------------")    
+    print(f"{line99Text} {winner}")
+    print(lineBreak)    
 
-# Write to analysis file
-with open(outputfile, "w", encoding="UTF8") as poll_analysisfile:
-    writer = csv.writer(poll_analysisfile)
-    writer.writerow(["Election Results"])
-    writer.writerow(["Total Votes:", numVoters])
-## How to output names?
-
-    writer.writerow(["Winner:", winner])
+    outputLine = line99Text + winner
+    ptext.writelines(outputLine)
+    ptext.write('\n')
+    ptext.writelines(lineBreak)
 
 # End File
